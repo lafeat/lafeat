0c63fbad171fe84921c928c7c7d3b472d6afe09c
Xitong Gao <gxtfmx@gmail.com>
2020-11-22 13:15:17 +0000

    Better convert boundaries

--- convert.py
+++ convert.py
@@ -2,8 +2,6 @@ import os
 import argparse
 
 import torch
-from torch.utils import data
-from torchvision import transforms, datasets
 import numpy as np
 
 
@@ -13,6 +11,7 @@ def parse_args():
         '--data', type=str, default='attacks/cifar10_X.npy')
     parser.add_argument('--save-dir', type=str, default='attacks')
     parser.add_argument('--name', type=str, default='lafeat.pt')
+    parser.add_argument('--epsilon', type=str, default='0.031')
     return parser.parse_args()
 
 
@@ -20,22 +19,39 @@ def permute(x):
     return x.permute(0, 2, 3, 1).contiguous()
 
 
-def convert(args):
-    x = np.load(args.data)
-    epsilon = 0.031
+def epsilon(s):
     # Different platforms and PyTorch/Numpy versions
     # could potentially give different
     # floating-point rounding errors,
-    # this following line is a hack
-    # to ensure boundary within epsilon.
-    # epsilon -= np.finfo(np.float32(1.0)).eps
+    # this following hack tightens the epsilon
+    # to ensure boundary within [-epsilon, epsilon].
+    e = np.float32(s)
+    return e - 4 * e * np.finfo(e).eps
+
+
+def convert(args):
+    x = np.float32(np.load(args.data))
+    e = epsilon(args.epsilon)
     load_path = os.path.join(args.save_dir, args.name)
-    xadv = torch.load(load_path)['adv_complete'].float()
+    xadv = torch.load(load_path)['adv_complete']
     xadv = permute(xadv)
-    d = torch.clamp(xadv - x, -epsilon, epsilon)
-    xadv = torch.clamp(d + x, 0.0, 1.0)
+    # d = torch.clamp(xadv - x, -e, e)
+    # xadv = torch.clamp(d + x, 0.0, 1.0)
+    xadv = xadv.numpy()
+    # xadv = np.clip(xadv, x - e, x + e)
+    d = np.clip(xadv - x, -e, e)
+    xadv = np.clip(d + x, 0.0, 1.0)
     save_path = os.path.join(args.save_dir, 'cifar10_X_adv.npy')
-    np.save(save_path, xadv.numpy())
+    np.save(save_path, xadv)
+    verbose(args.epsilon, xadv, x)
+
+
+def verbose(eps, xadv, x):
+    d = xadv - x
+    oe = np.float64(eps)
+    print(
+        f'{d.min()}, {d.max()}, '
+        f'True: {d.min() >= -oe}, True: {d.max() <= oe}.')
 
 
 if __name__ == '__main__':
